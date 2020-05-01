754380822ed35169c86ce06ce3bae9506c342c4d
Manoj Kumar <mkumariitdelhi@gmail.com>
2020-05-02 00:00:18 +0530

    backend of page2 completed

--- page2.js
+++ page2.js
@@ -1,12 +1,11 @@
 function postToGoogle() {
-                var field1 = $("#college").val();
-                var field2 = $("#CollegeState").val();
-                var field3 = $("#startTime").val();
-                var field4 = $("#endTime").val();
+                var college = $("#college").val();
+                var CollegeState = $("#CollegeState").val();
+                var startTime = $("#startTime").val();
+                var endTime = $("#endTime").val();
                 var height = $("#heightfeet").val() + "\'" + $("#heightinch").val() + "\"";
-                var field6 = $("#heightinch").val();
-                var weight1 = $("#weight").val();
-                var field7;// = document.getElementById("placeToTravel").value;
+                var weight = $("#weight").val();
+                var place;// = document.getElementById("placeToTravel").value;
                 var rd1 = document.getElementById("rd1");
                 var rd2 = document.getElementById("rd2");
                 var rd3 = document.getElementById("rd3");
@@ -14,53 +13,53 @@ function postToGoogle() {
 
                 if(rd1.checked==true)
                 {
-                    field7 = document.getElementById("rd1").value;
+                    place = document.getElementById("rd1").value;
                 }
                 else if(rd2.checked==true)
                 {
-                    field7 = document.getElementById("rd2").value;
+                    place = document.getElementById("rd2").value;
                 }
                 else if(rd3.checked==true)
                 {
-                    field7 = document.getElementById("rd3").value;
+                    place = document.getElementById("rd3").value;
                 }
                 else if(rd4.checked==true)
                 {
-                    field7 = document.getElementById("rd4").value;
+                    place = document.getElementById("rd4").value;
                 }
                 else {
                     alert("Please select one option!");
                     return false;
                 }
-                var field8 = $("#addline1").val();
-                var field9 = $("#addline2").val();
-                var field10 = $("#presentState").val();
-                var field11 = $("#pinCode").text();
-                var address = field8 + ", " + field9 + ", STATE - " + field10 + ", PIN - " + field11;
-                // var height = field5 + "' " + field6;
-                // var field4 = $("#placeToTravel option:selected").text();
+                var addline1 = $("#addline1").val();
+                var addline2 = $("#addline2").val();
+                var presentState = $("#presentState").val();
+                var pinCode = $("#pinCode").text();
         
-        // if(field1 == ""){
-        //   alert('Please Fill university details');
-        //   document.getElementById("college").focus();
-        //   return false;
-        // }
-        // if(field2 == ""){
-        //   alert('Please Fill State');
-        //   document.getElementById("CollegeState").focus();
-        //   return false;
-        // }
-        // if(field7 == "")
-        // {
-        //   alert('Please select one option');
-        //   document.getElementById("placeToTravel").focus();
-        //   return false;
-        // }
+        if(college == ""){
+          alert('Please Fill university details');
+          document.getElementById("college").focus();
+          return false;
+        }
+        if(CollegeState == ""){
+          alert('Please Fill State');
+          document.getElementById("CollegeState").focus();
+          return false;
+        }
+        if(place == "")
+        {
+          alert('Please select one option');
+          document.getElementById("place").focus();
+          return false;
+        }
         
   
                 $.ajax({
-                    url: "https://docs.google.com/forms/d/e/1FAIpQLSdn0C__bhtufyDheFgx6BfYhvyuL6EumgD62mze5M0ZJhbZdQ/formResponse?",
-          data: {"entry.630525043": field1, "entry.707736545": field2, "entry.1786621969": field3, "entry.393785043": field4, "entry.1789715397": height, "entry.1736541269": field7},
+                    url: "https://docs.google.com/forms/d/e/1FAIpQLSe_cNCavXzTl-78Ze0c3vdr30_xMKXNgAcf8JrBMYeGNVYdVA/formResponse?",
+          data: {"entry.1256704265": college, "entry.591722219": CollegeState, "entry.484089851": startTime,
+           "entry.1473222291": endTime, "entry.460946191": height,
+           "entry.349050934": weight, "entry.349050934": place, "entry.611337322": addline1,
+            "entry.274257086": addline2, "entry.1217447060": presentState, "entry.1892486679":pinCode},
 
 
              // data: {"entry.630525043": field1, "entry.707736545": field2, "entry.1786621969": field3, "entry.393785043": field4, "entry.393785043": height, "entry.1736541269":field7, "entry.1397015985":address},
@@ -72,7 +71,7 @@ function postToGoogle() {
           error: function(x, y, z)
             {
 // var result = str.link("https://www.w3schools.com");
-              $('#success-msg').show();
+              $("data Colleced!").show();
               $('#form').hide();
               window.open("page3.html", "_top");
             }
